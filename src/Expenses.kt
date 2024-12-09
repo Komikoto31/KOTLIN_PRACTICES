64f74bd762c2debaa1d60ecad9a393c6550a10b7
Komikoto <danyashihov98@gmail.com>
2024-12-09 14:17:26 +0300

    emove unnecessary files

--- src/Expenses.kt
+++
@@ -1,6 +0,0 @@
-open class Expenses(val AmountExpense: Int, val Category: String, val Data: Int){
-
-    fun Display() {
-        println("Сумма расхода: $AmountExpense, Категория: $Category, Дата: $Data")
-    }
-}
