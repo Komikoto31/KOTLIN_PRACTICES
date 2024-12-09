26fdcac536f63a57c1a7de1fcdee319cf270485a
Komikoto <danyashihov98@gmail.com>
2024-12-09 19:45:16 +0300

    DEL

--- IdeaProjects/Project_1/src/Lab_9/Nameable.java
+++
@@ -1,6 +0,0 @@
-package Lab_9;
-
-// Интерфейс, представляющий объект с именем
-interface Nameable {
-    String getName();
-}
--- IdeaProjects/Project_1/src/Lab_9/Priceable.java
+++
@@ -1,6 +0,0 @@
-package Lab_9;
-
-// Интерфейс, представляющий объект с ценой
-interface Priceable {
-    double getPrice();
-}
